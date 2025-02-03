8761d0b43c1dc551fb5ecc4f568ac661d1c1cabd
ExelansKa <uyeliklerimicinyahu@hotmail.com>
2025-02-03 21:14:41 +0300

    added Tabcontrol Module project

---
+++ Modules/PrismTabApp.Modules.TabContent/TabContentModule.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Events;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Navigation.Regions;
+using PrismTabApp.Events;
+using PrismTabApp.Modules.TabContent.Views;
+using System;
+
+namespace PrismTabApp.Modules.TabContent
+{
+    public class TabContentModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+        private readonly IEventAggregator _eventAggregator;
+        public TabContentModule(IRegionManager regionManager, IEventAggregator eventAggregator)
+        {
+            _regionManager = regionManager;
+            _eventAggregator = eventAggregator;
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _eventAggregator.GetEvent<Events.TabAddedEvent>().Subscribe(ShowView, true);
+        }
+
+        private void ShowView(TabAddedEvent.Model model)
+        {
+            // Event geldiğinde view'ı belirli bir region'a yerleştir
+            _regionManager.RequestNavigate("MainRegion", "TabContentView");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // View'ı kaydedin
+            containerRegistry.RegisterForNavigation<TabContentView>("TabContentView");
+        }
+    }
+}
---
+++ Modules/PrismTabApp.Modules.TabContent/ViewModels/TabContentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Navigation.Regions;
+using PrismTabApp.Core.Mvvm;
+using PrismTabApp.Services.Interfaces;
+
+namespace PrismTabApp.Modules.TabContent.ViewModels
+{
+    public class TabContentViewModel : RegionViewModelBase
+    {
+        private readonly IRegionManager _regionManager;
+        private readonly IMessageService _messageService;
+        private string _message = "Test Message...";
+
+        public TabContentViewModel(IRegionManager regionManager, IMessageService messageService) : base(regionManager)
+        {
+            this._regionManager = regionManager;
+            this._messageService = messageService;
+            Message = this._messageService.GetMessage();
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+    }
+}
